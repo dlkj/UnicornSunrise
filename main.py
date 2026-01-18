8c58dac7f4e640a425fc77c7f189a81c77b5a49b
Daniel Kenyon-Jones <github@dlkj.co.uk>
2026-01-18 18:13:47 +0000

    Start of mvc app structure

--- main.py
+++ main.py
@@ -10,271 +10,117 @@
 # Clock synchronizes time on start, and resynchronizes if you press the A button
 
 import time
-import math
-import machine
-import network
-import ntptime
+
+# import math
+# import machine
+# import network
+# import ntptime
 from galactic import GalacticUnicorn
-from picographics import PicoGraphics, DISPLAY_GALACTIC_UNICORN as DISPLAY
-
-try:
-    from secrets import WIFI_SSID, WIFI_PASSWORD
-    wifi_available = True
-except ImportError:
-    print("Create secrets.py with your WiFi credentials to get time from NTP")
-    wifi_available = False
+from picographics import PicoGraphics, DISPLAY_GALACTIC_UNICORN
 
 
-# constants for controlling the background colour throughout the day
-MIDDAY_HUE = 1.1
-MIDNIGHT_HUE = 0.8
-HUE_OFFSET = -0.1
+class Display:
+    def __init__(
+        self, gu: GalacticUnicorn, graphics: PicoGraphics, width: int, height: int
+    ):
+        self.gu = gu
+        self.graphics = graphics
+        self.width = width
+        self.height = height
+        self.white_pen = graphics.create_pen(255, 255, 255)
+        self.black_pen = graphics.create_pen(0, 0, 0)
 
-MIDDAY_SATURATION = 1.0
-MIDNIGHT_SATURATION = 1.0
-
-MIDDAY_VALUE = 0.8
-MIDNIGHT_VALUE = 0.3
-
-
-# create galactic object and graphics surface for drawing
-gu = GalacticUnicorn()
-graphics = PicoGraphics(DISPLAY)
-
-# create the rtc object
-rtc = machine.RTC()
-
-width = GalacticUnicorn.WIDTH
-height = GalacticUnicorn.HEIGHT
-
-# set up some pens to use later
-WHITE = graphics.create_pen(255, 255, 255)
-BLACK = graphics.create_pen(0, 0, 0)
-
-
-@micropython.native  # noqa: F821
-def from_hsv(h, s, v):
-    i = math.floor(h * 6.0)
-    f = h * 6.0 - i
-    v *= 255.0
-    p = v * (1.0 - s)
-    q = v * (1.0 - f * s)
-    t = v * (1.0 - (1.0 - f) * s)
-
-    i = int(i) % 6
-    if i == 0:
-        return int(v), int(t), int(p)
-    if i == 1:
-        return int(q), int(v), int(p)
-    if i == 2:
-        return int(p), int(v), int(t)
-    if i == 3:
-        return int(p), int(q), int(v)
-    if i == 4:
-        return int(t), int(p), int(v)
-    if i == 5:
-        return int(v), int(p), int(q)
-
-
-# function for drawing a gradient background
-def gradient_background(start_hue, start_sat, start_val, end_hue, end_sat, end_val):
-    half_width = width // 2
-    for x in range(0, half_width):
-        hue = ((end_hue - start_hue) * (x / half_width)) + start_hue
-        sat = ((end_sat - start_sat) * (x / half_width)) + start_sat
-        val = ((end_val - start_val) * (x / half_width)) + start_val
-        colour = from_hsv(hue, sat, val)
-        graphics.set_pen(graphics.create_pen(int(colour[0]), int(colour[1]), int(colour[2])))
-        for y in range(0, height):
-            graphics.pixel(x, y)
-            graphics.pixel(width - x - 1, y)
-
-    colour = from_hsv(end_hue, end_sat, end_val)
-    graphics.set_pen(graphics.create_pen(int(colour[0]), int(colour[1]), int(colour[2])))
-    #print(f'{colour}')
-    for y in range(0, height):
-        graphics.pixel(half_width, y)
-
-def black_background():
-    graphics.set_pen(BLACK)
-    for x in range(0, width):
-        for y in range(0, height):
-            graphics.pixel(x, y)
-
-
-def white_background():
-    graphics.set_pen(WHITE)
-    for x in range(0, width):
-        for y in range(0, height):
-            graphics.pixel(x, y)
-
-
-# function for drawing outlined text
-def outline_text(text, x, y, brightness = 255):
-    graphics.set_pen(BLACK)
-    graphics.text(text, x - 1, y - 1, -1, 1)
-    graphics.text(text, x, y - 1, -1, 1)
-    graphics.text(text, x + 1, y - 1, -1, 1)
-    graphics.text(text, x - 1, y, -1, 1)
-    graphics.text(text, x + 1, y, -1, 1)
-    graphics.text(text, x - 1, y + 1, -1, 1)
-    graphics.text(text, x, y + 1, -1, 1)
-    graphics.text(text, x + 1, y + 1, -1, 1)
-
-    text_pen = graphics.create_pen(brightness, brightness, brightness)
-    graphics.set_pen(text_pen)
-    graphics.text(text, x, y, -1, 1)
-
-
-# Connect to wifi and synchronize the RTC time from NTP
-def sync_time():
-    # if not wifi_available:
-    #     return
-
-    # Start connection
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
-    wlan.config(pm=0xa11140)  # Turn WiFi power saving off for some slow APs
-    wlan.connect(WIFI_SSID, WIFI_PASSWORD)
-
-    # Wait for connect success or failure
-    max_wait = 1000
-    while max_wait > 0:
-        if wlan.status() < 0 or wlan.status() >= 3:
-            break
-        max_wait -= 1
-        print('waiting for connection...')
-        time.sleep(0.5)
-
-        # redraw_display()
-        white_background()
-        gu.update(graphics)
-
-    if max_wait > 0:
-        print("Connected")
-
-        try:
-            ntptime.settime()
-            print("Time set")
-        except OSError:
-            pass
-
-    wlan.disconnect()
-    wlan.active(False)
-
-
-# NTP synchronizes the time to UTC, this allows you to adjust the displayed time
-# by one hour increments from UTC by pressing the volume up/down buttons
-#
-# We use the IRQ method to detect the button presses to avoid incrementing/decrementing
-# multiple times when the button is held.
-utc_offset = 0
-
-up_button = machine.Pin(GalacticUnicorn.SWITCH_VOLUME_UP, machine.Pin.IN, machine.Pin.PULL_UP)
-down_button = machine.Pin(GalacticUnicorn.SWITCH_VOLUME_DOWN, machine.Pin.IN, machine.Pin.PULL_UP)
-
-
-def adjust_utc_offset(pin):
-    global utc_offset, last_second
-    if pin == up_button:
-        utc_offset += 1
-        last_second = None
-    if pin == down_button:
-        utc_offset -= 1
-        last_second = None
-
-
-up_button.irq(trigger=machine.Pin.IRQ_FALLING, handler=adjust_utc_offset)
-down_button.irq(trigger=machine.Pin.IRQ_FALLING, handler=adjust_utc_offset)
-
-
-year, month, day, wd, hour, minute, second, _ = rtc.datetime()
-
-last_second = second
-
-
-def adjust_brightness():
-    light = gu.light()
-    target = max(0.01, min(1.0, (light/200.0)-.05))
-    brightness = gu.get_brightness()
-
-    #print(f'light: {light}, target: {target}, bright: {brightness}')
-
-    if abs(brightness - target) > 0.05:
-        if  brightness > target:
-            gu.adjust_brightness(-0.005)
-        else:
-            gu.adjust_brightness(0.005)
-
-
-def draw_clock_text(hour, minute, second, utc_offset):
-        hour = (hour + utc_offset) % 12
-        clock = "{:2}:{:02}".format(hour, minute)
-        # clock = "{:2}:{:02}:{:02}".format(hour, minute, second)
-
-        # calculate text position so that it is centred
-        w = graphics.measure_text(clock, 1)
-        x = int(width / 2 - w / 2 + 1)
+    def draw_text_screen(self, text: str):
+        self.graphics.set_pen(self.black_pen)
+        self.graphics.clear()
+        self.graphics.set_pen(self.white_pen)
+        draw_width = self.graphics.measure_text(
+            text, scale=1, spacing=1, fixed_width=False
+        )
+        x = int(self.width / 2 - draw_width / 2 + 1)
         y = 2
+        self.graphics.text(
+            text, x, y, wordwrap=-1, scale=1, spacing=1, angle=0, fixed_width=False
+        )
 
-        print(f'{gu.light()}')
-        if gu.light() < 20:
-            brightness = 50
-        else:
-            brightness = 255
+    def set_min_brightness(self, b: float):
+        if self.gu.get_brightness() < b:
+            self.gu.set_brightness(b)
 
-        outline_text(clock, x, y, brightness)
+    def update(self):
+        self.gu.update(self.graphics)
 
-def redraw_display():
-    global year, month, day, wd, hour, minute, second, last_second
-    year, month, day, wd, hour, minute, second, _ = rtc.datetime()
 
-    if hour == 12 and minute == 59 and second == 30:
-        if second != last_second:
-            sync_time()
+class ClockController:
+    def __init__(self, gu: GalacticUnicorn, display: Display):
+        self.gu = gu
+        self.display = display
 
-    if wd <= 4 and ((hour == 6 and minute >= 45) or (hour == 7 and minute < 15)):
-        #sunrise
-        if second != last_second:
-            # Convert to minutes since 06:45
-            minutes_since_start = (hour - 6) * 60 + minute - 45
-            # Total duration is 30 minutes (06:45 to 07:15)
-            rise_progress = minutes_since_start / 30.0
+    def initialise(self):
+        raise WifiException()
 
-            gu.set_brightness(rise_progress)
-            hue = 1.1
-            sat = 1.0 - rise_progress
-            val = rise_progress
-            gradient_background(hue, sat, val,
-                            hue + HUE_OFFSET, sat, val)
-            draw_clock_text(hour, minute, second, utc_offset)
-    elif wd <= 4 and ((hour == 7 and minute >= 15 and minute < 45)):
-        if second != last_second:
-            gu.set_brightness(1.0)
-            white_background()
-            draw_clock_text(hour, minute, second, utc_offset)
-    else:
-        #clock
-        adjust_brightness()
-        if second != last_second:
-            black_background()
-            draw_clock_text(hour, minute, second, utc_offset)
+    def clock(self):
+        pass
 
-    last_second = second
+    def error(self):
+        self.display.set_min_brightness(0.1)
+        self.display.draw_text_screen("Error")
+        self.display.update()
 
-# set the font
-graphics.set_font("bitmap8")
-gu.set_brightness(0.1)
+    def wifi_error(self):
+        self.display.set_min_brightness(0.1)
+        self.display.draw_text_screen("WiFi Error")
+        self.display.update()
 
-sync_time()
 
-while True:
-    if gu.is_pressed(GalacticUnicorn.SWITCH_A):
-        sync_time()
+class WifiException(Exception):
+    pass
 
-    redraw_display()
 
-    # update the display
-    gu.update(graphics)
+class ClockApp:
+    def __init__(self, gu: GalacticUnicorn, display: Display):
+        self.controller = ClockController(gu, display)
 
-    time.sleep(0.01)
+    def run(self):
+        try:
+            self.controller.initialise()
+            while True:
+                self.controller.clock()
+                time.sleep(0.01)
+        except WifiException:
+            self.controller.wifi_error()
+        except Exception:
+            self.controller.error()
+
+
+# class ClockModel:
+#     def __init__(self):
+#         pass
+
+
+# class ClockDisplayView:
+#     def __init__(self):
+#         pass
+
+
+# class ClockDisplayViewModel:
+#     def __init__(self):
+#         pass
+
+
+# class InitialiseView:
+#     def __init__(self):
+#         pass
+
+
+# class InitialiseViewModel:
+#     def __init__(self):
+#         pass
+
+gu = GalacticUnicorn()
+graphics = PicoGraphics(DISPLAY_GALACTIC_UNICORN)
+
+display = Display(gu, graphics, GalacticUnicorn.WIDTH, GalacticUnicorn.HEIGHT)
+
+app = ClockApp(gu, display)
+app.run()
